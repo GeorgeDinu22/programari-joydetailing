f4786ce422d81458169d434648f5c9f1668f2c57
George Dinu <georgedinu2210@gmail.com>
2026-06-18 12:31:33 +0300

    Update V2

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,8 +3,8 @@ import "./globals.css";
 
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://www.joydetailing.ro"),
-  title: "Joy Detailing – Servicii Profesionale de Detailing Auto",
+  metadataBase: new URL("https://programari.joydetailing.ro"),
+  title: "Programează Acum la Joy Detailing – Detailing Auto Profesional",
   description:
     "Joy Detailing oferă servicii complete de detailing auto: exterior, interior, motor, polish faruri și semi-detailing. Curățare, polish, protecție și întreținere pentru ca mașina ta să arate impecabil și să fie protejată pe termen lung.",
   keywords: [
@@ -26,13 +26,13 @@ export const metadata: Metadata = {
       "Joy Detailing – Redă strălucirea mașinii tale cu servicii profesionale de îngrijire auto",
     description:
       "La Joy Detailing, tratăm fiecare mașină cu atenție și pasiune, oferindu-i un aspect impecabil și protecție de durată. Redescoperă plăcerea condusului ca în prima zi!",
-    url: "https://www.joydetailing.ro/",
+    url: "https://programari.joydetailing.ro/",
     siteName: "Joy Detailing",
     type: "website",
     locale: "ro_RO",
     images: [
       {
-        url: "https://www.joydetailing.ro/whatsapp.jpg",
+        url: "https://programari.joydetailing.ro/whatsapp.jpg",
         width: 1200,
         height: 935,
         alt: "Joy Detailing – servicii profesionale de detailing auto",
